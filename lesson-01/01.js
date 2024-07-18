02482d6b668822160e0c3abe60678683767124cb
DmitriiLevchenko <lev11081989@gmail.com>
2024-07-18 22:07:27 +1000

    comlete 01_02

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -2,9 +2,9 @@
 // Результат присвойте переменной sum
 
 // your code
-let sum;
-let n = 1;
+let sum = 0;
+let n = 0;
 while (n < 20) {
-        sum += n
-        n += 1
-    };
+    n += 1
+    sum += n
+};
