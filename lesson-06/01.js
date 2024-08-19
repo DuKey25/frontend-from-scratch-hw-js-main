00df4d8f0279138c8c9c6501d9397cb7519538b0
DmitriiLevchenko <lev11081989@gmail.com>
2024-08-19 23:06:55 +1000

    complete 06_01

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -17,3 +17,41 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const prevButton = document.querySelector('#prev-button')
+const nextButton = document.querySelector('#next-button')
+
+
+
+
+
+prevButton.addEventListener('click', function () {
+  const img = document.querySelector('#web-tech-image')
+  for (let i = 0; i < WEB_TECH_IMAGES.length; i++) {
+    if (img.src === WEB_TECH_IMAGES[i]) {
+      if (i === 0) {
+        img.src = WEB_TECH_IMAGES[WEB_TECH_IMAGES.length - 1]
+        break
+      } else {
+        img.src = WEB_TECH_IMAGES[i - 1]
+        break
+      }
+    }
+  }
+})
+
+
+nextButton.addEventListener('click', function () {
+  const img = document.querySelector('#web-tech-image')
+  for (let i = 0; i < WEB_TECH_IMAGES.length; i++) {
+    if (img.src === WEB_TECH_IMAGES[i]) {
+      if (i === WEB_TECH_IMAGES.length - 1) {
+        img.src = WEB_TECH_IMAGES[0]
+        break
+      } else {
+        img.src = WEB_TECH_IMAGES[i + 1]
+        break
+      }
+    }
+  }
+})
